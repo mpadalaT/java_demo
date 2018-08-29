c1c69b53e660e8b3a02abdefc6e167cb8abce3e8
mpadalaT <mpadalax@hotmail.com>
2018-08-29 12:21:32 +0530

    Iniial Project Commit

---
+++ src/main/java/Demo/app.java
@@ -0,0 +1,8 @@
+package Demo;
+
+public class app {
+
+	public String sample() {		
+		return "Hello";
+	}
+}
---
+++ src/test/java/Demo/AppTest.java
@@ -0,0 +1,15 @@
+package Demo;
+//clean compile test package
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
